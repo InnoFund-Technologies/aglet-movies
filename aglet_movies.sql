42b792e3cfdea0a0cd74341f77c5cde49ca0a8c7
BrendonChirume <chirumebrendon7@gmail.com>
2025-02-26 04:06:44 +0200

    Update database name in SQL dump for Aglet Movies project

--- aglet_movies.sql
+++ aglet_movies.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.41, for Linux (x86_64)
 --
--- Host: localhost    Database: aglet_brief_laravel
+-- Host: localhost    Database: aglet_movies
 -- ------------------------------------------------------
 -- Server version	8.0.41-0ubuntu0.24.04.1
 
